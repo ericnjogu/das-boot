d3caf1772fc1a929df92abeed0c42b9e2ef7b5a2
Eric Njogu <kunadawa@gmail.com>
2019-07-05 10:02:49 +0300

    test: adding test that checks the JSON output

--- src/test/java/com/mugowanjogu/dasboot/controller/ShipwreckControllerIntegrationTest.java
+++ src/test/java/com/mugowanjogu/dasboot/controller/ShipwreckControllerIntegrationTest.java
@@ -5,20 +5,31 @@ package com.mugowanjogu.dasboot.controller;
 
 import static org.junit.Assert.fail;
 
+import java.io.IOException;
 import java.util.Collection;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mugowanjogu.dasboot.model.Shipwreck;
 
 /**
@@ -26,9 +37,11 @@ import com.mugowanjogu.dasboot.model.Shipwreck;
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ShipwreckControllerIntegrationTest {
 	
+	@LocalServerPort
+	private int port;
 	@Autowired
 	private ShipwreckController controller;
 
@@ -47,6 +60,23 @@ public class ShipwreckControllerIntegrationTest {
 		Collection<Shipwreck> list = controller.list();
 		assertThat(list, is(notNullValue()));
 		assertThat(list.size(), is(greaterThan(0)));
+	}
+	
+	/**
+	 * Test method for {@link com.mugowanjogu.dasboot.controller.ShipwreckController#list()}.
+	 * @throws IOException 
+	 */
+	@Test
+	public void testList_REST() throws IOException {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/api/v1/wrecks/all", String.class);
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode responseJson = objectMapper.readTree(response.getBody());
+		
+		assertThat(responseJson.isMissingNode(), is(false));
+		assertThat(responseJson.iterator().next().get("condition").toString(), not(isEmptyOrNullString()));
 	}
 
 	/**
